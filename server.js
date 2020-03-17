5d7648bc4ef785e414e3228bef99cdc8bf9cdcdb
Cody Brewer <brewer.cody.m@gmail.com>
2020-03-17 00:34:21 -0600

    express.json, helmet, cors, and logger middlware

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const { logger } = require('./middleware/index');
 
 const server = express();
 
+// Middleware:
+// Built-In
+server.use(express.json());
+// Third party
+server.use(helmet());
+server.use(cors());
+// Custom
+server.use(logger);
+
 server.get('/', (req, res) => {
-  res.status(200).json({ message: "<h2>Let's write some middleware!</h2>" });
+  // eslint-disable-next-line quotes
+  res.status(200).send(`<h2>Let's write some middleware!</h2>`);
 });
 
 module.exports = server;
